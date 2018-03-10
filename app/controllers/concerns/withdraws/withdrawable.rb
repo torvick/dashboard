8536649aa8e65b18c3e3459cb0204564c941b6c8
Dave Fletcher <graveyhead@gmail.com>
2018-03-10 11:33:34 -0500

    Reroll pull request 43 against stable branch.

--- app/controllers/concerns/withdraws/withdrawable.rb
+++ app/controllers/concerns/withdraws/withdrawable.rb
@@ -40,6 +40,7 @@ module Withdraws
     end
 
     def withdraw_params
+      params[:withdraw][:fee] = channel.fee
       params[:withdraw][:currency] = channel.currency
       params[:withdraw][:member_id] = current_user.id
       params.require(:withdraw).permit(:fund_source, :member_id, :currency, :sum)
